5a7228bf3fdcdc552b4a97c0f74adf1cb68462b9
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-04 21:55:03 -0500

    demo#3 working

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -16,4 +16,14 @@ Notes:
 
 def multiply_nums(nums):
     # Your code here
+    # list comprehension
+    nums = nums.split(", ")
+    product = 1
+    for num in nums:
+        product *= int(num)
+    return product
 
+print(multiply_nums("2, 3"))# ➞ 6
+print(multiply_nums("1, 2, 3, 4"))# ➞ 24
+print(multiply_nums("54, 75, 453, 0"))# ➞ 0
+print(multiply_nums("10, -2"))# ➞ -20
