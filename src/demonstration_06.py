919af61ed0362daee81b68a4424d76399e031dc8
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-04 22:26:03 -0500

    demo06.py

--- src/demonstration_06.py
+++ src/demonstration_06.py
@@ -9,4 +9,12 @@ The input string will only consist of lower case letters and/or spaces.
 """
 def get_count(input_str):
     # Your code here
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    vowel_ctr = 0
+    for letter in input_str:
+        if letter in vowels:
+            vowel_ctr += 1
+    return vowel_ctr
+
+print(get_count("aaaaaaaa"))
 
