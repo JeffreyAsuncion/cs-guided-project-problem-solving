23d0ec842a9fa12736359b4624e1b37f619283bd
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-04 21:29:36 -0500

    demo#2.py

--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -13,8 +13,16 @@ Examples:
 Notes:
 - The input list will only contain integers.
 """
-
+import numpy as np
 
 def add_indexes(numbers):
     # Your code here
+    new_lst = []
+    for i, element in enumerate(numbers):
+        new_lst.append(int(i)+int(element))
 
+    return new_lst
+
+print(add_indexes([0, 0, 0, 0, 0]))# ➞ [0, 1, 2, 3, 4]
+print(add_indexes([1, 2, 3, 4, 5]))# ➞ [1, 3, 5, 7, 9]
+print(add_indexes([5, 4, 3, 2, 1]))# ➞ [5, 5, 5, 5, 5]
